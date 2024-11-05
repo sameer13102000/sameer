5ff21e26452d2d2f9b641c371397d88caa91aea6
admin <sameerburli10@gmail.com>
2024-11-05 19:22:55 +0530

    XML Config

---
+++ src/main/java/com/sameer/sameer/Laptop.java
@@ -0,0 +1,15 @@
+package com.sameer.sameer;
+
+public class Laptop {
+
+    private int count;
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public Laptop(){
+        System.out.println("Laptop Object created");
+    }
+}
